f6f746c28e567025e60aab47f12658e422c687d8
shonsirsha <seangeekpro@gmail.com>
2022-06-25 20:59:47 +0200

    Basic setup and scripts created

---
+++ index.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", async () => {
+	try {
+		const BLOCKED_URLS = [
+			"https://i.instagram.com/api/v1/stories/reel/seen",
+			"https://i.instagram.com/api/v1/direct_v2/threads/*",
+		];
+		const getFromStorage = () => {
+			return { cancel: true };
+		};
+		chrome.webRequest.onBeforeRequest.addListener(
+			function () {
+				return getFromStorage();
+			},
+			{
+				urls: BLOCKED_URLS,
+			},
+			["blocking"]
+		);
+	} catch (e) {
+		console.error(e);
+	}
+});
