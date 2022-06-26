43d3349b03fe0f240f4b48fe76590429c61bf526
shonsirsha <seangeekpro@gmail.com>
2022-06-26 13:34:32 +0200

    UI developed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 	try {
 		const BLOCKED_URLS = [
 			"https://i.instagram.com/api/v1/stories/reel/seen",
-			"https://i.instagram.com/api/v1/direct_v2/threads/*",
+			"https://i.instagram.com/api/v1/direct_v2/threads/*/items/*/seen/",
 		];
 		const getFromStorage = () => {
 			return { cancel: true };
